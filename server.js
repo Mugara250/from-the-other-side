625ca11028cffa16bc420a57227a3b1967288f64
Mugara250 <mbonyumugara52@gmail.com>
2025-09-25 11:06:48 +0200

    import.meta

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ import http from "node:http";
 const PORT = 8000;
 
 const server = http.createServer((request, response) => {
-    response.setHeader("Content-Type", "text/html");
-    response.statusCode = 200;
-    // response.writeHead(200, "OK", {"content-type": "text/html"});
-    response.end("<html><h1>The server is working</h1></html>");
+  response.setHeader("Content-Type", "text/html");
+  response.statusCode = 200;
+  // response.writeHead(200, "OK", {"content-type": "text/html"});
+  response.end("<html><h1>The server is working</h1></html>");
 });
 
+const dirname = import.meta.dirname; // absolute path of the directory containing the currently opened module
+const filename = import.meta.filename; // absolute path to the currently opened module
 
 server.listen(PORT, "localhost", () => {
-    console.log(`Server has started listening on PORT ${PORT}`);
-})
+  console.log(`Server has started listening on PORT ${PORT}`);
+});
